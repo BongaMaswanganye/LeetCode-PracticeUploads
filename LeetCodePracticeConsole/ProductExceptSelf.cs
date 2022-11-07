1c2660799f5a4558f13cc3c1dc55888c12a300a3
Bonga Maswanganye <snowmehn@gmail.com>
2022-11-06 22:19:26 -0500

    im so behind i gotta do more monday

--- LeetCodePracticeConsole/ProductExceptSelf.cs
+++ LeetCodePracticeConsole/ProductExceptSelf.cs
@@ -7,6 +7,30 @@ namespace LeetCodePracticeConsole
 {
     public class ProductExceptSelf
     {
-        // need to do tomorrow too tired
+        public int[] ProductExceptSelf(int[] nums) {
+            //if null or empty do nothing
+            if (nums == null || nums.Length == 0)
+                return null;
+                //create holder array for storing
+            int[] result = new int[nums.Length];
+            //temp value to hold fake spot
+            int temp = 1;
+            //multiply to the right of each variable and place it in equivalent holder spot
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = temp;
+                temp *= nums[i];
+            }
+            //reset temp to one
+            temp = 1;
+            //multiply everything to the left of a number and put it in its appropriate start
+            for (int i = result.Length - 1; i >= 0; i--)
+            {
+                result[i] *= temp;
+                temp *= nums[i];
+            }
+            //return holder
+            return result;
+        }
     }
 }
